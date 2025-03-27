b988d1915bc11f3f1709d33fbfb57a6048a8625b
unknown <tangweihao5@gmail.com>
2025-03-27 21:32:56 +0800

    Add minor adjustments and checks

--- Backend/code/idealpointestimates.py
+++ Backend/code/idealpointestimates.py
@@ -4,9 +4,6 @@ import pandas as pd
 countryCodes = pd.read_csv("../data/COW country codes.csv") # Checked, 0 missing value
 agreementScores = pd.read_csv("../data/AgreementScoresAll_Jun2024.csv")
 
-targetCountry = "Singapore"
-targetCountryCode = countryCodes[countryCodes["StateNme"] == targetCountry]["CCode"].iloc[0]
-
 # Adding in country names to data
 agreementScoresMerged = pd.merge(agreementScores, countryCodes, left_on = "ccode1", right_on = "CCode", how = "inner")
 agreementScoresMerged = agreementScoresMerged.drop(columns = ["CCode"])
@@ -16,18 +13,23 @@ agreementScoresMerged = pd.merge(agreementScoresMerged, countryCodes, left_on = 
 agreementScoresMerged = agreementScoresMerged.drop(columns = ["CCode"])
 agreementScoresMerged.rename(columns = {"StateAbb":"StateAbb2", "StateNme":"StateName2"}, inplace = True)
 
-# Filtering for data relevant to Singapore only
+# Filtering for data relevant only to Singapore
 agreementScoresMerged = agreementScoresMerged[(agreementScoresMerged["StateName1"] == "Singapore") | (agreementScoresMerged["StateName2"] == "Singapore")]
 
-# removing duplicate rows (ccode1, ccode2) vs (ccode2, ccode1)
-# need to check if this removed correctly, because data size went from 22,448 to 9,834 observations, way less than half :(
+# Removing duplicate entries. Checked that no ('ccode1', 'ccode2', 'year') combination have different IdealPointDistance
+agreementScoresMerged.drop_duplicates(
+    subset=['ccode1', 'ccode2', 'year', 'IdealPointDistance'],
+    inplace=True
+)
+
+# Removing duplicate symmetric rows - (ccode1, ccode2) and (ccode2, ccode1)
 agreementScoresMerged['sorted_pair'] = agreementScoresMerged.apply(
     lambda row: tuple(sorted([row['ccode1'], row['ccode2']])), axis=1
 )
 agreementScoresMerged = agreementScoresMerged.drop_duplicates(subset=['sorted_pair', 'IdealPointDistance', 'year', 'session.x'])
 agreementScoresMerged = agreementScoresMerged.drop(columns=['sorted_pair'])
 
-# making ccode1 and StateName1 always Singapore
+# Making ccode1 and StateName1 always refer to Singapore
 def ensure_singapore_first(row):
     if row['StateName1'] != "Singapore":
         # Swap values
@@ -37,7 +39,5 @@ def ensure_singapore_first(row):
 
 agreementScoresMerged = agreementScoresMerged.apply(ensure_singapore_first, axis=1)
 
-# agreementScoresMerged.groupby('year').size()
-# can see which year we want to start from, based on number of observations
-
-#print(agreementScoresMerged.head())
+# Keeping only relevant columns
+agreementScoresMerged = agreementScoresMerged[['StateName1', 'StateName2', 'year', 'IdealPointDistance']]
