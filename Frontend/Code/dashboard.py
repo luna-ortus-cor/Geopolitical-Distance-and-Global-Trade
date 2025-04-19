8c7b7308b726d44726bd39dd0fee1a5dc9681b7f
rliu1119 <liurong0123@outlook.com>
2025-04-19 15:22:39 +0800

    deployment test

--- Frontend/Code/dashboard.py
+++ Frontend/Code/dashboard.py
@@ -678,4 +678,4 @@ def update_recommendations(clickData):
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(host = '0.0.0.0', port=8080, debug=False)
+    app.run(host = '0.0.0.0', port=8080, debug=False)
